765c7239a4578c0ecb43cbb20eae8279da552401
ieatacid <ieatacid@gmail.com>
2017-12-30 13:13:02 -0500

    Initial commit

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Add custom commands to terminal view"""
+plugin_description = """Lets you add custom command buttons to the terminal tab"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "ieatacid"
